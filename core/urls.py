cebea9919a0fc274e8793be657ecd3e89db71ad1
Vinniewizard <vincentkipkoech818@gmail.com>
2026-05-20 19:06:18 +0300

    Fix Css

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as static_serve
 
 from accounts.views import dashboard, logout_view, register
 from django.contrib.auth import views as auth_views
@@ -14,3 +15,8 @@ urlpatterns = [
     path("investments/", include("investments.urls")),
     path("payments/", include("payments.urls")),
 ]
+
+if settings.DEBUG or not getattr(settings, "HAS_WHITENOISE", False):
+    urlpatterns += [
+        re_path(r"^static/(?P<path>.*)$", static_serve, {"document_root": settings.STATICFILES_DIRS[0]}),
+    ]
