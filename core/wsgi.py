fe536abdb64a07b4d0bd6f3101a8ae55151ddfca
Vinniewizard <vincentkipkoech818@gmail.com>
2026-05-24 15:51:08 +0300

    Standardize Vercel build using package.json hooks and simplify routing

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the project root to the path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
